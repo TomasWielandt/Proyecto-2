3dba5090cb45a4d108fbc1ae15dc4d37c5e348c4
TomasWielandt <twielandt16@gmail.com>
2024-05-02 18:06:43 -0400

    Se agregaron mas comentarios explicando al código

--- pf/index.js
+++ pf/index.js
@@ -1,5 +1,7 @@
-//Creación de la variable encuesta que contendrá un arreglo de preguntas y respuestas
+//Creación de la variable encuestas que contendrá un arreglo de preguntas y opciones de respuestas
 const encuestas = [];
+
+//Creación de variable respuestasd que contendrá un arreglo de preguntas y respuestas seleccionadas
 const respuestas = [];
 
 //Función que crea un objeto que contendrá una pregunta y dos opciones de respuesta
@@ -15,7 +17,10 @@ let crearEncuesta = () => {
     opcion2: opcion2
   };
   
+//Método push para ingresar los resultados a la variable encuestas
   encuestas.push(encuesta);
+
+//Condición if que muestra mensajes con los numeros de preguntas ingresadas
    if (encuestas.length < 8){
     console.log(`Debe ingresar al menos 8 preguntas, a ingresado solo ${encuestas.length}`)
 } else {
@@ -26,9 +31,15 @@ let crearEncuesta = () => {
 
 //Función que imprimprimirá las preguntas y que permitira elegir entre las 2 opciones
 let crearRespuesta = () => {
+
+//Condición if que perimite hacer la pregunta y elegir la respuesta siempre y cuando hayan
+//por lo menos 8 preguntas
     if (encuestas.length >= 8){
         encuestas.forEach((encuesta, index) => {
             let opcion;
+
+//Bucle do while que sirve para repetir la pregunta si es que el usuario ingresa una 
+//respuesta no válida
             do{
                 opcion = prompt(`${index + 1}. ${encuesta.pregunta}: ${encuesta.opcion1} o ${encuesta.opcion2}`).toLowerCase(); 
                 if (opcion === encuesta.opcion1){
@@ -36,20 +47,25 @@ let crearRespuesta = () => {
                     pregunta: encuesta.pregunta,
                     respuesta: encuesta.opcion1
                 };
+
+//Método push para ingresar los resultados a la variable respuestas
                respuestas.push(respuesta);
                 } else if (opcion === encuesta.opcion2){
                 let respuesta = {
                     pregunta: encuesta.pregunta,
                     respuesta: encuesta.opcion2
                 };
+
+//Método push para ingresar los resultados a la variable respuestas                
                respuestas.push(respuesta);
-                } else {alert("Respuesta no valida")}
+                } else {alert("Respuesta no válida")}
             } while (opcion !== encuesta.opcion1 && opcion !== encuesta.opcion2);
                 return opcion;
         });
     } else {console.log("Tiene que haber minimo 8 preguntas para responder la encuesta")}
 };
 
+//Función para imprimir los resultados 
 function resultados() {
     let resultadosString = "Resultado de la encuesta:\n";
     respuestas.forEach((respuesta, index) => {
